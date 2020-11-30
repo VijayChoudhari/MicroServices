9a88e47fee85cfeeefafc884136851d55d04251e
VijayChoudhari <vijay_d_choudhari@yahoo.com>
2020-11-30 23:14:19 +1100

    Bing Search

---
+++ BingSearch/Controller/BingSearchController.cs
@@ -0,0 +1,42 @@
+﻿using BingSearh.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BingSearch.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BingSearchController : ControllerBase
+    {
+        private readonly ISearchLogic _searchLogic;
+
+        public BingSearchController(ISearchLogic searchLogic)
+        {
+            _searchLogic = searchLogic;
+        }
+
+        //http://localhost:55036/api/bingsearch/getsearchoccurences/e-settlements/www.sympli.com.au
+        [HttpGet]
+        [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Any)]
+        [Route("getSearchOccurences/{searchToken}/{url}")]
+        public IActionResult GetSearchOccurences(string searchToken, string url)
+        {
+            int occurenceCount = 0;
+            try
+            {
+                occurenceCount = _searchLogic.GetUrlOccurencesBySearchToken(searchToken, url);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+            return Ok(occurenceCount);
+        }
+    }
+}
---
+++ BingSearch/Startup.cs
@@ -0,0 +1,73 @@
+using BingSearch.Repository;
+using BingSearh.Logic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BingSearch
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddResponseCaching();
+
+            services.AddSingleton<ISearchRepository>(new SearchRepository());
+            IServiceCollection serviceCollections = services.AddSingleton<ISearchLogic>(sl => new SearchLogic(services.BuildServiceProvider().GetService<ISearchRepository>(),
+                                                                                            services.BuildServiceProvider().GetService<IConfiguration>()));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseResponseCaching();
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.GetTypedHeaders().CacheControl =
+                    new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+                    {
+                        Public = true,
+                        MaxAge = TimeSpan.FromSeconds(3600)
+                    };
+                context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] =
+                    new string[] { "Accept-Encoding" };
+
+                await next();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
